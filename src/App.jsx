3054294d782ee8dbadd2467148c258ab140c5a73
Jashimuddin123 <adnanmahmud3394@gmail.com>
2024-07-06 02:05:13 +0600

    install react dom and setup react router

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+ 
+
+  return (
+    <>
+      
+      <h1 className="text-4xl text-center text-purple-800">Vite + React</h1>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBrs8rx18riKipWzasnZIcGkQETP60uazA",
+  authDomain: "asian-traveler-website.firebaseapp.com",
+  projectId: "asian-traveler-website",
+  storageBucket: "asian-traveler-website.appspot.com",
+  messagingSenderId: "1081025689071",
+  appId: "1:1081025689071:web:ca263df2ec56620612321a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
