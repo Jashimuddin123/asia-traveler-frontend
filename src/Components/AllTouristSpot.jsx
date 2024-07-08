28445a3daa1ac5f04c48527917739ba0993646fc
Jashimuddin123 <adnanmahmud3394@gmail.com>
2024-07-08 13:08:00 +0600

    update touristspot design

--- src/Components/AllTouristSpot.jsx
+++ src/Components/AllTouristSpot.jsx
@@ -3,17 +3,20 @@ import { Link } from "react-router-dom";
 
 const AllTouristSpot = () => {
   const [allTourist, setAllTourist] = useState([]);
+  console.log('all tourist here', allTourist);
+
   useEffect(() => {
     fetch("http://localhost:5000/touristSpots")
       .then((res) => res.json())
       .then((data) => setAllTourist(data));
   }, []);
+
   return (
     <div>
-      <h1 className="text-5xl text-center font-bold">all tourist spot here</h1>
-      {allTourist.length}
+      <h1 className="text-5xl text-center font-bold">All tourist spot here</h1>
+      <div className="text-center text-3xl font-bold"> {allTourist.length}</div>
       <div className="grid grid-cols-1 mx-auto max-w-5xl">
-        {allTourist.map((all, index) => (
+        {allTourist.map((tourist, index) => (
           <li className="decoration-red-700" key={index}>
             <div className="max-w-4xl mx-auto p-6">
               <div className="bg-white shadow-md rounded-lg overflow-hidden">
@@ -23,17 +26,17 @@ const AllTouristSpot = () => {
                       Bay of Bengal
                     </div>
                     <h2 className="text-2xl font-bold mt-2">
-                      Saint Martin Island
+                      {tourist.tourist_spot_name}
                     </h2>
                     <div className="mt-4 flex items-center space-x-4">
                       <span className="bg-blue-500 text-white px-3 py-1 rounded-full">
-                        3 days & 2 nights
+                      {tourist.travel_time}
                       </span>
                       <span className="bg-blue-500 text-white px-3 py-1 rounded-full">
-                        $320
+                        {tourist.average_cost}
                       </span>
                       <span className="bg-blue-500 text-white px-3 py-1 rounded-full">
-                        Spring
+                        {tourist.seasonality}
                       </span>
                     </div>
                     <p className="mt-4 text-gray-700">
@@ -42,31 +45,29 @@ const AllTouristSpot = () => {
                       sandy beaches.
                     </p>
                     <p className="mt-2 text-gray-700 font-bold">
-                      Visitors Per Year: 300001
+                      Visitors Per Year: {tourist.total_visitors}
                     </p>
                     <div className="mt-4 flex items-center space-x-2">
                       <img
-                        className="w-6 h-6 rounded-full"
-                        src="https://via.placeholder.com/24"
+                        className="w-10 h-10 rounded-full"
+                        src="https://i.ibb.co/99w123q/mu-img.jpg"
                         alt="Author"
                       />
-                      <span className="text-gray-700">
-                        by Kamruzzaman Bayezid
-                      </span>
+                      <span className="text-gray-700">by Jashim Uddin</span>
                     </div>
                   </div>
                   <div className="w-1/3">
                     <img
-                      className="h-full w-full object-cover"
-                      src="https://via.placeholder.com/300"
-                      alt="Saint Martin Island"
+                      className="h-full w-full lg:w-72 lg:h-64  rounded-lg object-cover"
+                      src={tourist.photo_url}
+                      alt={tourist.tourist_spot_name}
                     />
                   </div>
                 </div>
                 <div className="bg-gray-100 text-blue-500 px-6 py-4">
-                   
-                    <Link to={`/touristDetails/${all._id}`}> <button>Show Details</button> </Link>
-              
+                  <Link to={`/touristDetails/${tourist._id}`}>
+                    <button>Show Details</button>
+                  </Link>
                 </div>
               </div>
             </div>
@@ -77,5 +78,4 @@ const AllTouristSpot = () => {
   );
 };
 
-//   /* <Link to={}><button className="btn btn-secondary">show details</button></Link> */
 export default AllTouristSpot;
--- src/Components/TouristSpotsSection.jsx
+++ src/Components/TouristSpotsSection.jsx
@@ -1,5 +1,11 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { TbCards } from "react-icons/tb";
+import { FiCloudOff } from "react-icons/fi";
+import { FaLocationDot } from "react-icons/fa6";
+
+
+
 
 const TouristSpotsSection = () => {
   const [touristSpots, setTouristSpots] = useState([]);
@@ -12,46 +18,61 @@ const TouristSpotsSection = () => {
   }, []);
 
   return (
-    <div>
-      <h2 className="text-5xl text-center"> {touristSpots.length}</h2>
+    <div className="container mx-auto p-4">
+      <h2 className="text-5xl text-center mb-6">{touristSpots.length}</h2>
       {touristSpots.length === 0 ? (
         <p className="text-5xl text-center font-bold">Loading...</p>
       ) : (
-        <ul>
-          <div className="grid grid-cols-3 mx-auto max-w-5xl h-[700px]">
-            {touristSpots.slice(0, 6).map((spot, index) => (
-              <li key={index}>
-                <div className="card bg-base-100 w-80 shadow-xl gap-y-4">
-                  <figure className="px-10 pt-10">
-                    <img
-                      src={spot.photo_url}
-                      alt="Shoes"
-                      className="rounded-xl"
-                    />
-                  </figure>
-                  <div className="card-body items-center text-center">
-                    <h2 className="card-title">{spot.country_name}</h2>
-                    <p>{spot.tourist_spot_name}</p>
-                    <div className="card-actions">
-                      <button className="btn btn-primary">Buy Now</button>
-                    </div>
-                  </div>
-                </div>
-                {/* Add other fields as necessary */}
-              </li>
-              
-            ))}
-            
-            <Link to="/allTouristSpot">
-              <button className=" w-44 mx-auto  btn btn-warning ">
-                View All Tourist Spot
-              </button>
-            </Link>
-   
-          </div>
+        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6">
+          {touristSpots.slice(0, 6).map((spot, index) => (
+            <li key={index}>
+              <div className="card bg-base-100 w-full h-80 shadow-xl">
+                <figure className="px-10 pt-10">
+                  <img 
+                  className="w-72 h-96 rounded-xl"
+                    src={spot.photo_url}
+                    alt={spot.tourist_spot_name}
+                   
+                  />
+                </figure>
+                <div className="card-body items-center text-center">
+                <p className="text-3xl mt-0 font-bold ">{spot.tourist_spot_name}</p>
          
+                  {/* <h2 className="card-title">{spot.country_name}</h2> */}
+            
+                     <span className="flex gap-3 items-center "> 
+                          <p><FaLocationDot /></p>
+                     <p>{spot.location}</p>
+                     </span>
+                     
+                     <div className="flex gap-10 ">
+                        <span className="flex gap-3 items-center">
+                          <p className="text-orange-500"><TbCards /></p>
+                        <p className="text-red-800">{spot.average_cost}</p>
+                   
+                        </span>
+                            
+                       <div>
+                        <span className="flex gap-3 items-center">
+                        <FiCloudOff />
+                        <p>{spot.seasonality}</p>
+                        </span>
+                       </div>
+
+                     </div>
+                </div>
+              </div>
+            </li>
+          ))}
         </ul>
       )}
+      <div className="flex justify-center mt-6">
+        <Link to="/allTouristSpot">
+          <button className="btn btn-warning w-full md:w-auto">
+            View All Tourist Spots
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
